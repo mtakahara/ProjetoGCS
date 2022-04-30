0eb880f44459df8b3776e1ff233303e392355d20
Marcos Bernardi <marcos200.22@gmail.com>
2022-04-30 11:50:38 -0300

    adiciona metodos listarUsuarios e atualizaUsuarios

--- App/src/SingletonBD.java
+++ App/src/SingletonBD.java
@@ -1,17 +1,38 @@
 import java.util.ArrayList;
 
+
 public class SingletonBD {
 
     private static ArrayList<Usuario> listaUsuario = new ArrayList<Usuario>();
 
     private SingletonBD() { }
 
-    public static void adicionarUsuario(){ }
+    public static void adicionarUsuario(){
 
-    public static void deletarUsuario(){ }
+    }
 
-    public static void atualizarUsuario(){ }
+    public static void deletarUsuario(){
 
-    public static void listarUsuarios(){ }
+    }
+
+    public static void atualizarUsuario() {
+
+        System.out.println("Usuario atualizado \n");
+    }
+    public static void listarUsuarios(){
+
+        System.out.println("Lista de usuários: \n");
+
+        if (listaUsuario.size() != 0){
+        for (int i = 0; i < listaUsuario.size(); i++) {
+            System.out.print("Usuario: " + listaUsuario.get(i).getUsuario() + " |  ");
+            System.out.print("Senha: " + listaUsuario.get(i).getSenha() + " |  ");
+            System.out.print("Nome: " + listaUsuario.get(i).getNome() + " |  ");
+            System.out.print("Ano de Nascimento: " + listaUsuario.get(i).getAnoNascimento() + "\n\n");
+        }
+        } else{
+            System.out.println("Lista Vazia \n");
+        }
+    }
 
 }
