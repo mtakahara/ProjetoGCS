38d879e3cef82a81ceb993f6930cb2f461ae6371
Marcos Bernardi <marcos200.22@gmail.com>
2022-04-30 09:43:33 -0300

    criacao de opcao de menu

--- App/src/Main.java
+++ App/src/Main.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("***Menu***");
         System.out.println("(1) Cadastrar Usuário");
         System.out.println("(2) Deletar Usuário");
         System.out.println("(3) Atualizar  Usuário");
         System.out.println("(4) Listar Usuário");
-        System.out.println("(5) Cadastrar Usuário");
+        System.out.println("(5) Sair");
+
+        System.out.println("Opção: ");
+        int opcao = sc.nextInt();
 
 
     }
