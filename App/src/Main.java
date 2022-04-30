a7533d804a6d5c431ff3c9d9e8e2354817442a7a
Marcos Bernardi <marcos200.22@gmail.com>
2022-04-30 10:22:10 -0300

    adicionado as classes main, Singleton e Usuario do zip

--- App/src/Main.java
+++ App/src/Main.java
@@ -3,18 +3,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        int opcao = 0;
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("***Menu***");
-        System.out.println("(1) Cadastrar Usuário");
-        System.out.println("(2) Deletar Usuário");
-        System.out.println("(3) Atualizar  Usuário");
-        System.out.println("(4) Listar Usuário");
-        System.out.println("(5) Sair");
+        while(opcao != 9) {
 
-        System.out.println("Opção: ");
-        int opcao = sc.nextInt();
+            System.out.println("**** MENU ****");
+            System.out.println("(1) Cadastrar Usuário");
+            System.out.println("(2) Listar Usuario");
+            System.out.println("(3) Deletar Usuário");
+            System.out.println("(4) Atualizar Usuário\n");
+            System.out.println("(9) Sair\n");
+            System.out.print("Opção: ");
+            opcao = sc.nextInt();
 
-
+            if(opcao == 1) {
+                SingletonBD.adicionarUsuario();
+            }
+            else if(opcao == 2) {
+                SingletonBD.deletarUsuario();
+            }
+            else if(opcao == 3) {
+                SingletonBD.listarUsuarios();
+            }
+            else if(opcao == 4) {
+                SingletonBD.atualizarUsuario();
+            }
+            else if(opcao == 9) {
+                System.exit(0);
+            }
+            else{
+                System.out.println("\n\n\n\n\n\n\n\n\n");
+            }
+        }
     }
 }
--- App/src/SingletonBD.java
+++ App/src/SingletonBD.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
+
 public class SingletonBD {
 
+    private static ArrayList<Usuario> listaUsuario = new ArrayList<Usuario>();
+
+    private SingletonBD() { }
+
+    public static void adicionarUsuario(){ }
+
+    public static void deletarUsuario(){ }
+
+    public static void atualizarUsuario(){ }
+
+    public static void listarUsuarios(){ }
 
 }
